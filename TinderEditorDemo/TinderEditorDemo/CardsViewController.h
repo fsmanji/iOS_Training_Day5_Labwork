53930f6adbb2dc4d330bd51aca4a984165440fea
Cristan <cristanz@yahoo-inc.com>
2015-09-18 12:42:38 -0700

    Setup the projects.

---
+++ TinderEditorDemo/TinderEditorDemo/CardsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardsViewController.h
+//  TinderEditorDemo
+//
+//  Created by Cristan Zhang on 9/18/15.
+//  Copyright (c) 2015 FastTrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardsViewController : UIViewController
+
+@end
---
+++ TinderEditorDemo/TinderEditorDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TinderEditorDemo
+//
+//  Created by Cristan Zhang on 9/18/15.
+//  Copyright (c) 2015 FastTrack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
